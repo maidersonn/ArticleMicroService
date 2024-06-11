f811aff7d48eec69ea38c63b6685a9679f7068ea
Maider Landaribar <maiderlandaribar@Maiders-MacBook-Pro.local>
2024-06-11 10:08:12 +0200

    add validation

--- src/main/java/com/maider/article/config/KafkaConsumerConfig.java
+++ src/main/java/com/maider/article/config/KafkaConsumerConfig.java
@@ -3,26 +3,32 @@ package com.maider.article.config;
 import com.maider.article.domain.entities.dto.UserDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.KafkaListenerConfigurer;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistrar;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @EnableKafka
 @Configuration
-public class KafkaConsumerConfig {
+public class KafkaConsumerConfig  implements KafkaListenerConfigurer {
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
     @Value(value = "S{spring.kafka.consumer.groupId}")
     private String groupId;
+    @Autowired
+    private LocalValidatorFactoryBean validator;
     @Bean
     public ConsumerFactory<String, UserDTO> userConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
@@ -49,5 +55,10 @@ public class KafkaConsumerConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(userConsumerFactory());
         return factory;
+    }
+
+    @Override
+    public void configureKafkaListeners(KafkaListenerEndpointRegistrar registrar) {
+        registrar.setValidator(this.validator);
     }
 }
